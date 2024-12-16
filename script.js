9b029938b83556057a531b9ccd524adede144c7b
hank.yu <hank.yu@appier.com>
2024-12-16 14:57:17 +0800

    wip

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('actionButton').addEventListener('click', () => {
+    const message = document.getElementById('message');
+    message.textContent = 'You clicked the button!';
+});
