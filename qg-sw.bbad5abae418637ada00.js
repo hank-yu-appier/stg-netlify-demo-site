0698654394ad864967412facd31dd149dbe9951d
hank.yu <hank.yu@appier.com>
2025-06-05 01:44:22 +0800

    wip

--- qg-sw.bbad5abae418637ada00.js
+++ qg-sw.bbad5abae418637ada00.js
@@ -1,2 +1,2 @@
 var appId = "bbad5abae418637ada00";
-importScripts("https://cdn.qgraph.io/v3/r/qg-sw.js");
+importScripts("https://9ebb-2402-7500-506-a890-e9c8-5635-a71e-38ee.ngrok-free.app/qg-sw.js");
